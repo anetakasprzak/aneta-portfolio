280e9b9edc9dd3796ba115fe1f470dc4aca18b72
Aneta Kasprzak <anet.kasprzak@gmail.com>
2024-02-15 10:51:53 +0000

    changing static skills data into dynamic

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,9 +1,25 @@
 import "./Skills.css";
 
+const skills = [
+  { id: 1, icon: "devicon-html5-plain-wordmark" },
+  { id: 2, icon: "devicon-css3-plain-wordmark" },
+  { id: 3, icon: "devicon-sass-original" },
+  { id: 4, icon: "devicon-javascript-plain" },
+  { id: 5, icon: "devicon-react-original-wordmark" },
+  { id: 6, icon: "devicon-vscode-plain-wordmark" },
+  { id: 7, icon: "devicon-github-original-wordmark" },
+  { id: 8, icon: "devicon-figma-plain" },
+];
+
 export default function Skills() {
   return (
     <section className="skills__section">
-      <p className="skill">
+      {skills.map((skill) => (
+        <p className="skill" key={skill.id}>
+          <i className={skill.icon}></i>
+        </p>
+      ))}
+      {/* <p className="skill">
         <i className="devicon-html5-plain-wordmark"></i>
       </p>
       <p className="skill">
@@ -26,7 +42,7 @@ export default function Skills() {
       </p>
       <p className="skill">
         <i className="devicon-figma-plain"></i>
-      </p>
+      </p> */}
     </section>
   );
 }
